fc1505ed1f68fa8bf2736434b744620b28e913fb
Karanbir Chahal <karanbleep@gmail.com>
2019-10-31 16:55:38 -0400

    Setup stuff

---
+++ setup.sh
@@ -0,0 +1,9 @@
+conda install flask-socketio
+conda install flask-cors
+conda install scipy
+conda install pillow
+conda install bcolz
+conda install beautifulsoup4
+conda install h5py
+conda install imageio
+conda install pytorch torchvision -c pytorch
